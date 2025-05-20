f65038516d72d1d3ba7dd4ae8079fb049dbe6deb
unknown <patrickkeita80@yahoo.com>
2025-05-20 02:47:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongodb = require('./routes/data/database.js'); 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("Helo World")
 });
 
-app.listen(process.env.PORT ||  3000, () => {
-    console.log('Web Server is listening at port ' + (process.env.PORT || 3000 ));
+app.use('/', require('./routes/index.js'));
+
+app.use('/', require('./routes/contacts.js'));
+
+
+mongodb.initdb(function(err) {
+    if (err) { 
+        console.log(err);     
+    } else { 
+        app.listen(port, () => { 
+            console.log(`Database is listening and Node running on port ${port}`); 
+        }); 
+    } 
 });
+

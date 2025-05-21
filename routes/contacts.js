0d95287084bea7e543debaf737bb39794d70049c
unknown <patrickkeita80@yahoo.com>
2025-05-21 03:36:33 -0700

    Update contacts.js

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,7 +5,7 @@ const { ObjectId } = require('mongodb');
 const { getDatabase } = require('./data/database.js');
 
 
-router.get('/contacts', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const db = getDatabase();
         const contacts = await db.collection('Contacts').find().toArray();
@@ -16,7 +16,7 @@ router.get('/contacts', async (req, res) => {
     }
 });
 
-router.get('/contacts/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
         const db = getDatabase();
         const contact = await db.collection('Contacts').findOne({ _id: new ObjectId(req.params.id) });
@@ -28,6 +28,85 @@ router.get('/contacts/:id', async (req, res) => {
         res.json(contact);
     } catch (err) {
         console.error('Error fetching contact:', err);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const db = getDatabase();
+        const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+
+        if (!firstName || !lastName || !email || !favoriteColor || !birthday) {
+            return res.status(400).send('All fields are required.');
+        }
+
+        const result = await db.collection('Contacts').insertOne({
+            firstName,
+            lastName,
+            email,
+            favoriteColor,
+            birthday
+        });
+
+        res.status(201).json({ id: result.insertedId });
+    } catch (err) {
+        console.error('Error creating contact:', err);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+router.put('/:id', async (req, res) => {
+    try {
+        const db = getDatabase();
+        const contactId = new ObjectId(req.params.id);
+        const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+
+        if (!firstName || !lastName || !email || !favoriteColor || !birthday) {
+            return res.status(400).send('All fields are required.');
+        }
+
+        const result = await db.collection('Contacts').updateOne(
+            {_id: contactId},
+            {
+                $set: {
+                    firstName,
+                    lastName,
+                    email,
+                    favoriteColor,
+                    birthday
+                }
+            }
+        );
+
+        if (result.matchedCount === 0) {
+            return res.status(404).send('Contact not found.');
+        }
+
+        res.status(204).send();
+    } catch (err) {
+        console.error('Error updating contact:', err);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const db = getDatabase();
+
+        const contactId = new ObjectId(req.params.id);
+
+        const result = await db.collection('Contacts').deleteOne({ _id: contactId });
+
+        if (result.deletedCount === 0) {
+            return res.status(404).send('Contact not found.');
+        }
+
+        res.status(200).send('Contact deleted.');
+    } catch (err) {
+        console.error('Error deleting contact:', err);
         res.status(500).send('Server Error');
     }
 });
